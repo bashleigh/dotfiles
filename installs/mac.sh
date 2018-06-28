9d62ca7cfc78df22f97b92073a8ffc9be497a28b
ashleigh <ashleigh@wi-q.com>
2018-06-28 16:23:02 +0100

    Added docker setup file
    Signed-off-by: ashleigh <ashleigh@wi-q.com>

--- installs/mac.sh
+++ installs/mac.sh
@@ -10,7 +10,7 @@ if ! [ -x "$(command -v brew)" ]; then
     exit 1;
 fi
 
-declare -a commands=("git" "gpg" "zsh", "zsh-completions", "node", "neofetch", "thefuck", "wget", "docker", "docker-compose", "docker-machine", "xhyve", "docker-machine-driver-xhyve", "docker-machine-nfs", "hub")
+declare -a commands=("git" "gpg" "zsh", "zsh-completions", "node", "neofetch", "thefuck", "wget", "docker", "docker-compose", "docker-machine", "xhyve", "docker-machine-driver-xhyve", "docker-machine-nfs", "hub", "ngrok")
 
 for i in "${commands[@]}"
 do
@@ -21,4 +21,10 @@ done
 
 if ! [ -x "$(command -v iterm2)"]
     brew cask install iterm2 
+fi
+
+brew cask install vmware-fusion
+
+if [ ! -f "~/Library/Fonts/Meslo LG M Regular for Powerline.ttf"]; then 
+    wget -O "~/Library/Fonts/Meslo LG M Regular for Powerline.ttf" https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf?raw=true 
 fi
